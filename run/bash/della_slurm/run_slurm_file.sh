a373e4b1565a0d35ac50090b2f9de5eb1e23197e
Anjian Li <anjianli2001@gmail.com>
2023-11-22 01:18:14 -0500

    re-run all the file

--- run/bash/della_slurm/run_slurm_file.sh
+++ run/bash/della_slurm/run_slurm_file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Loop from 1 to 90 to submit each job
-for i in {91..126}
+for i in {1..126}
 do
    sbatch "/home/al5844/desktop/project/denoising-diffusion-pytorch/run/bash/della_slurm/slurm_job_$i.slurm"
 done
