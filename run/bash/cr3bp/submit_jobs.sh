121cf725cf3e2588b72d173add782d43ede48e96
Anjian Li <anjianli2001@gmail.com>
2024-01-25 11:24:25 -0500

    fix submit job file for cr3bp

--- run/bash/cr3bp/submit_jobs.sh
+++ run/bash/cr3bp/submit_jobs.sh
@@ -3,7 +3,7 @@
 # Loop to submit job_i.slurm for i in range(0, 14)
 sbatch run/bash/cr3bp/input_alpha_output_time_mass_control_num_300k.slurm
 sbatch run/bash/cr3bp/input_alpha_output_time_mass_num_300k.slurm
-sbbatch run/bash/cr3bp/input_alpha_time_mass_output_control_num_300k.slurm
+sbatch run/bash/cr3bp/input_alpha_time_mass_output_control_num_300k.slurm
 
 sbatch run/bash/cr3bp/input_alpha_output_time_mass_control_num_30k.slurm
 sbatch run/bash/cr3bp/input_alpha_output_time_mass_num_30k.slurm
