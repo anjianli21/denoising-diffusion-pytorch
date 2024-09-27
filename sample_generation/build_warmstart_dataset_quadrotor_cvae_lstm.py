085dd191707e135141378042c01bd358ddf3cb74
Anjian Li <anjianli2001@gmail.com>
2024-09-26 22:55:48 -0400

    add cvae lstm build dataset, sample script

--- sample_generation/build_warmstart_dataset_quadrotor_cvae_lstm.py
+++ sample_generation/build_warmstart_dataset_quadrotor_cvae_lstm.py
@@ -1,23 +1,30 @@
-import os
 import glob
-import re
-import time
 
-# TODO: here is improved constraint, sampled average violation
-from denoising_diffusion_pytorch.classifier_free_guidance_cond_1d_improved_constrained_diffusion import Unet1D, \
-    GaussianDiffusion1D, Trainer1D
+import sys
+import os
+
+sys.path.append('/home/anjian/Desktop/project/generative_trajectory_optimization')
+
+from models import *
+
 from denoising_diffusion_pytorch.constraint_violation_function_improved_quadrotor import \
     get_constraint_violation_quadrotor
 
 import copy
 import numpy as np
 import pickle
+import yaml
 import torch
 
+CVAE_PARENT_DIR = "results/iclr25/quadrotor/results/cvae_seed_2"
+RNN_PARENT_DIR = "results/iclr25/quadrotor/results/lstm_seed_2"
 
 def main():
 
-    # TODO: change time range
+
+    data_type_list = ["cvae_lstm_seed_2"]
+
+    # Setup v2
     TIME_MIN = 4.20
     TIME_MAX = 4.57
     CONTROL_U1_MIN = - np.pi / 9 - .001
@@ -41,49 +48,24 @@ def main():
 
     obs_num = 4
 
-    # sample_type_list = ["conditional_sample"]
-    sample_type = "full_sample"
-
-    # diffusion_w_list = [10.0, 5.0]
-    diffusion_w = 5.0
+    device = "cuda:0"
 
     sample_num = 10
     condition_seed_num = 20
 
     condition_seed_list = [5000 + i for i in range(condition_seed_num)]
 
-    data_type_list = [
-        # "quadrotor_diffusion_seed_0",
-        "quadrotor_diffusion_seed_1",
-        "quadrotor_diffusion_seed_2",
-    ]
-
-    # data_type_list = [
-    #     "quadrotor_constrained_diffusion_seed_0",
-    #     # "quadrotor_constrained_diffusion_seed_1",
-    #     # "quadrotor_constrained_diffusion_seed_2",
-    # ]
-
-    # Configure path ##############################################################################################
-    parent_path = f"results/iclr25/quadrotor/results"
-
-    # DDDAS
-    model_parent_path_list = [f"{parent_path}/{i}" for i in data_type_list]
-
     constraint_violation_list = []
     for i in range(len(data_type_list)):
         data_type = data_type_list[i]
-        model_parent_path = model_parent_path_list[i]
-
         current_prediction_data_list = []
 
-        obs_condition_goalperturb_input_list = []
+        obs_goalperturb_condition_input_list = []
         for j in range(len(condition_seed_list)):
             condition_seed = condition_seed_list[j]
 
             # Sample obs
             rng_condition = np.random.RandomState(seed=condition_seed)
-            # obs sample
             is_condition_reasonable = False
 
             # Sample condition
@@ -93,7 +75,8 @@ def main():
 
                 agent_start_pos = np.array([[-12.0, 0.0, 0.0]])
                 # TODO: add random perturbation to the goal pos
-                agent_goal_pos_perturbation = rng_condition.rand(1) * 2 * GOAL_PERTURBATION_RANGE - GOAL_PERTURBATION_RANGE
+                agent_goal_pos_perturbation = rng_condition.rand(
+                    1) * 2 * GOAL_PERTURBATION_RANGE - GOAL_PERTURBATION_RANGE
                 agent_goal_pos = np.array([[12., 0., 0.]]) + agent_goal_pos_perturbation
 
                 obs_radius = rng_condition.rand(obs_num) * (OBS_RADIUS_MAX - OBS_RADIUS_MIN) + OBS_RADIUS_MIN
@@ -140,35 +123,49 @@ def main():
             obs_pos = np.hstack([obs_pos_x.reshape(1, -1), obs_pos_y.reshape(1, -1), obs_pos_z.reshape(1, -1)])
 
             obs_condition_input = np.hstack([obs_pos, obs_radius])
-
             # Repeat the same obs input as the sample num
             obs_condition_input = np.tile(obs_condition_input, (sample_num, 1))
 
             # TODO: also normalize the goal pos perturbation
             agent_goal_pos_perturbation = (agent_goal_pos_perturbation + GOAL_PERTURBATION_RANGE) / (
-                        2 * GOAL_PERTURBATION_RANGE)
+                    2 * GOAL_PERTURBATION_RANGE)
             agent_goal_pos_perturbation = agent_goal_pos_perturbation.reshape(1, -1)
             agent_goal_pos_perturbation = np.tile(agent_goal_pos_perturbation, (sample_num, 1))
 
             # TODO: combine obs condition and goalperturb
-            obs_condition_goalperturb_input = np.hstack([obs_condition_input, agent_goal_pos_perturbation])
-            obs_condition_goalperturb_input = torch.tensor(obs_condition_goalperturb_input).float().cuda()
-            obs_condition_goalperturb_input_list.append(obs_condition_goalperturb_input)
+            obs_goalperturb_condition_input = np.hstack([obs_condition_input, agent_goal_pos_perturbation])
+            obs_goalperturb_condition_input = torch.tensor(obs_goalperturb_condition_input).float().cuda()
+            obs_goalperturb_condition_input_list.append(obs_goalperturb_condition_input)
 
-        obs_condition_goalperturb_input_list = torch.vstack(obs_condition_goalperturb_input_list)
+        obs_goalperturb_condition_input_list = torch.vstack(obs_goalperturb_condition_input_list)
 
-        if sample_type == "full_sample":
-            t_final_control_samples = sample_diffusion(condition_input=obs_condition_goalperturb_input_list,
-                                                       input_output_type="input_obs_goalperturb_output_t_control",
-                                                       checkpoint_parent_path=model_parent_path,
-                                                       sample_num=sample_num * condition_seed_num,
-                                                       diffusion_w=diffusion_w)
-            obs_condition_goalperturb_input_list = obs_condition_goalperturb_input_list.detach().cpu().numpy()
-            t_final_control_samples = t_final_control_samples.detach().cpu().numpy()
+        # First sample t_final using CVAE
+        t_final_samples = get_sample_from_vanilla_cvae(condition_input=obs_goalperturb_condition_input_list,
+                                                               sample_num=sample_num * condition_seed_num)
+        t_final_samples = t_final_samples.to(obs_goalperturb_condition_input_list.device)
 
-            obs_goalperturb_t_final_control_samples = np.hstack((obs_condition_goalperturb_input_list, t_final_control_samples))
+        obs_goalperturb_t_final_samples = torch.hstack([obs_goalperturb_condition_input_list, t_final_samples])
+
+        control_samples = get_sample_from_rnn(conditional_input=obs_goalperturb_t_final_samples)
+
+        obs_goalperturb_condition_input_list = obs_goalperturb_condition_input_list.detach().cpu().numpy()
+        t_final_samples = t_final_samples.detach().cpu().numpy()
+
+        obs_goalperturb_t_final_control_samples = np.hstack([obs_goalperturb_condition_input_list, t_final_samples, control_samples])
 
         current_prediction_data_list.append(copy.copy(obs_goalperturb_t_final_control_samples))
+
+        #############################################################################
+        # check constraint violation
+        current_prediction_data_list = np.vstack(current_prediction_data_list)
+        current_prediction_data_tensor = torch.tensor(current_prediction_data_list)
+        current_violation = get_constraint_violation_quadrotor(x=current_prediction_data_tensor[:, 17:],
+                                                               c=current_prediction_data_tensor[:, :17],
+                                                               scale=torch.tensor(1.0),
+                                                               device=current_prediction_data_tensor.device)
+        print(f"data type is {data_type}, violation is {current_violation}")
+
+        constraint_violation_list.append(current_violation)
 
         # Data preparation #######################################################################################################
         # obs_pos,
@@ -202,7 +199,7 @@ def main():
                                                                                 CONTROL_U3_MAX - CONTROL_U3_MIN) + CONTROL_U3_MIN
         print("data normalization is done")
 
-        # Save ##########################################################################################################
+        # Save as several file ###################################3
         total_num = sample_num * condition_seed_num
         for num in range(total_num):
             curr_conditional_seed = 5000 + num // 10
@@ -216,98 +213,10 @@ def main():
                 pickle.dump(obs_goalperturb_t_final_control_samples[num, :], f)
             print(f"{warmstart_data_path} is saved")
 
-        #############################################################################
-        # check constraint violation
-        current_prediction_data_list = np.vstack(current_prediction_data_list)
-        current_prediction_data_tensor = torch.tensor(current_prediction_data_list)
-        current_violation = get_constraint_violation_quadrotor(x=current_prediction_data_tensor[:, 17:],
-                                                               c=current_prediction_data_tensor[:, :17],
-                                                               scale=torch.tensor(1.0),
-                                                               device=current_prediction_data_tensor.device)
-        print(f"data type is {data_type}, violation is {current_violation}")
-
-        constraint_violation_list.append(current_violation)
-
     for i in range(len(constraint_violation_list)):
         print(f"{data_type_list[i]}, constraint violation {constraint_violation_list[i]}")
 
-
-def sample_diffusion(condition_input, input_output_type, checkpoint_parent_path, sample_num, diffusion_w):
-    # Initialize model ############################################################################
-    unet_dim = 128
-    unet_dim_mults = "4,4,8"
-    unet_dim_mults = tuple(map(int, unet_dim_mults.split(',')))
-    embed_class_layers_dims = "256,512"
-    embed_class_layers_dims = tuple(map(int, embed_class_layers_dims.split(',')))
-    timesteps = 500
-    objective = "pred_noise"
-    batch_size = 512
-    cond_drop_prob = 0.1
-
-    if input_output_type == "input_obs_goalperturb_output_t_control":
-        class_dim = 17
-        channel = 1
-        seq_length = 241
-    else:
-        print("wrong input output type")
-        exit()
-
-    model = Unet1D(
-        dim=unet_dim,
-        channels=channel,
-        dim_mults=unet_dim_mults,
-        embed_class_layers_dims=embed_class_layers_dims,
-        class_dim=class_dim,
-        cond_drop_prob=cond_drop_prob,
-        seq_length=seq_length
-    )
-
-    diffusion = GaussianDiffusion1D(
-        model=model,
-        seq_length=seq_length,
-        timesteps=timesteps,
-        objective=objective
-    ).cuda()
-
-    # read checkpoints ############################################################################
-    child_directories = glob.glob(os.path.join(checkpoint_parent_path, "*/*/"))
-    if child_directories:
-        final_result_folder = child_directories[0]
-
-    trainer = Trainer1D(
-        diffusion_model=diffusion,
-        dataset=[0, 0, 0],
-        results_folder=final_result_folder,
-    )
-    files = os.listdir(final_result_folder)
-    regex = r"model-epoch-(\d+).pt"  # Modified regex to match 1 or more digits
-    max_epoch = -1
-    milestone = ""
-    for file in files:
-        if file.endswith(".pt"):
-            match = re.search(regex, file)
-            if match:
-                epoch_num = int(match.group(1))
-                if epoch_num > max_epoch:
-                    max_epoch = epoch_num
-                    milestone = f"epoch-{epoch_num}"  # Format with leading zeros
-    # milestone = "epoch-102"
-    trainer.load(milestone)
-
-    # Sample results ################################################################################
-    # 3. Use the loaded model for sampling
-    start_time = time.time()
-    sample_results = diffusion.sample(
-        classes=condition_input.cuda(),
-        cond_scale=diffusion_w,
-    )
-    end_time = time.time()
-    print(f"{input_output_type}, {sample_num} data, takes {end_time - start_time} seconds")
-
-    sample_results = sample_results.reshape(sample_num, -1)
-
-    return sample_results
-
+    return True
 
 def check_condition(parameters, to_print=False):
     to_print = False
@@ -357,6 +266,75 @@ def check_condition(parameters, to_print=False):
 
     return True
 
+def get_sample_from_vanilla_cvae(sample_num, condition_input, seed=0):
+    parent_dir = CVAE_PARENT_DIR
+    cvae_config_path = parent_dir + "/version_0/config.yaml"
+    cvae_ckpt_path = parent_dir + "/version_0/training_stage_3/checkpoints/last.ckpt"
+
+    with open(cvae_config_path, 'r') as file:
+        try:
+            config = yaml.safe_load(file)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    model = generative_models[config['model_params']['model_name']](**config['model_params'],
+                                                                    **config['data_params'])
+
+    # https://pytorch-lightning.readthedocs.io/en/stable/deploy/production_intermediate.html
+    checkpoint = torch.load(cvae_ckpt_path, map_location="cpu")
+    model_weights = checkpoint["state_dict"]
+    for key in list(model_weights):
+        model_weights[key.replace("model.", "")] = model_weights.pop(key)
+    model.load_state_dict(model_weights)
+    model.eval()
+
+    curr_device = "cpu"
+
+    # Sample ###################################################################
+    # Sample x for alpha
+    sample_x_list = []
+
+    output_sample = model.sample(num_samples=sample_num, alpha=torch.tensor(condition_input),
+                                      current_device=curr_device, seed=seed)
+    # output_sample = output_sample.cpu().data.numpy()
+    #
+    # sample_data = np.squeeze(np.asarray(output_sample))
+    # np.random.shuffle(sample_data)
+
+    return output_sample.to("cuda:0")
+
+
+def get_sample_from_rnn(conditional_input):
+    rnn_parent_dir = RNN_PARENT_DIR
+    rnn_config_path = f"{rnn_parent_dir}/version_0/config.yaml"
+    rnn_ckpt_path = f"{rnn_parent_dir}/version_0/training_stage_3/checkpoints/last.ckpt"
+
+    with open(rnn_config_path, 'r') as file:
+        try:
+            config = yaml.safe_load(file)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    model = generative_models[config['model_params']['model_name']](**config['model_params'],
+                                                                    **config['data_params'])
+
+    # https://pytorch-lightning.readthedocs.io/en/stable/deploy/production_intermediate.html
+    checkpoint = torch.load(rnn_ckpt_path, map_location="cpu")
+    model_weights = checkpoint["state_dict"]
+    for key in list(model_weights):
+        model_weights[key.replace("model.", "")] = model_weights.pop(key)
+    model.load_state_dict(model_weights)
+    model.eval()
+
+    # curr_device = "cuda:0"
+    curr_device = "cpu"
+
+
+    # sample #########################################################################################################
+
+    [_, control] = model(input=torch.tensor(0.0).to(curr_device), alpha=conditional_input.to(curr_device), control_label=torch.tensor(0.0).to(curr_device))
+
+    return control.detach().cpu().numpy()
 
 if __name__ == "__main__":
     main()
